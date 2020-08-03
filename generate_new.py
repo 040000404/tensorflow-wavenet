c795bcecf5563707865d177a678e0904925481c0
Jade570 <chaelyoung@naver.com>
2020-08-03 16:41:15 +0900

    Add generate_new.py: dynamic temperature control

--- generate_new.py
+++ generate_new.py
@@ -5,6 +5,7 @@ import argparse
 from datetime import datetime
 import json
 import os
+import warnings
 
 import librosa
 import numpy as np
@@ -16,9 +17,9 @@ import dynamic_changer as dynamic
 
 SAMPLES = 16000
 TEMPERATURE = 1.0
-TEMP_CHANGE = "static"
+TEMP_CHANGE = None
 LOGDIR = './logdir'
-WAVENET_PARAMS = './wavenet_params.json'
+WAVENET_PARAMS = 'wavenet_params_new.json'
 SAVE_EVERY = None
 SILENCE_THRESHOLD = 0.1
 
@@ -107,15 +108,22 @@ def get_arguments():
         type=str,
         default=SILENCE_THRESHOLD,
         help='The threshold of silence.')
+    parser.add_argument(
+        '--silence_change',
+        type=str,
+        default=TEMP_CHANGE,
+        help='Change the silence dynamically during generation.')
 
     #additional arguments for dynamic_changer
-    parser.add_argument('--form', type=str, default=None)
-    parser.add_argument('--min', type=float, default=0)
-    parser.add_argument('--max', type=float, default=1)
-    parser.add_argument('--period', type=float, default=1)
-    parser.add_argument('--graph', type=str, default=None)
+    parser.add_argument('--tform', type=str, default=None)
+    parser.add_argument('--tmin', type=float, default=0)
+    parser.add_argument('--tmax', type=float, default=1)
+    parser.add_argument('--tperiod', type=float, default=1)
+    parser.add_argument('--tgraph', type=str, default=None)
 
     arguments = parser.parse_args()
+
+    # gc channel validation check
     if arguments.gc_channels is not None:
         if arguments.gc_cardinality is None:
             raise ValueError("Globally conditioning but gc_cardinality not "
@@ -125,15 +133,13 @@ def get_arguments():
         if arguments.gc_id is None:
             raise ValueError("Globally conditioning, but global condition was "
                               "not specified. Use --gc_id to specify global "
-                              "condition.")
-
+                              "condition.")    
     return arguments
 
 def write_wav(waveform, sample_rate, filename):
     y = np.array(waveform)
     librosa.output.write_wav(filename, y, sample_rate)
     print('Updated wav file at {}'.format(filename))
-
 
 def create_seed(filename,
                 sample_rate,
@@ -149,7 +155,6 @@ def create_seed(filename,
                         lambda: tf.constant(window_size))
 
     return quantized[:cut_index]
-
 
 def main():
     args = get_arguments()
@@ -252,14 +257,18 @@ def main():
 
         # temperature change by every 1/5 samples
 
-        if args.temperature_change == "static":
+        if args.temperature_change == None: #static
             _temp_temperature = args.temperature
-
         elif args.temperature_change == "dynamic":
-            if step % int(args.samples/5) == 0:
-                _temp_temperature = args.temperature * np.random.rand()
+            if args.tform == None: #random
+                if step % int(args.samples/5) == 0:
+                    _temp_temperature = args.temperature * np.random.rand()
+            elif args.tform == "sine": #sine
+                _temp_temperature = dynamic.sine(args.tmin, args.tmax, args.tperiod, step, args.samples)
+            elif args.tform == "square": #square
+                _temp_temperature = dynamic.square(args.tmin, args.tmax, args.tperiod, step, args.samples)                  
         else:
-            print("wrong temperature_change value")
+                raise Exception("wrong temperature_change value")
 
         scaled_prediction = np.log(prediction) / _temp_temperature
         scaled_prediction = (scaled_prediction -
@@ -269,11 +278,10 @@ def main():
 
         # Prediction distribution at temperature=1.0 should be unchanged after
         # scaling.
-        if args.temperature == 1.0:
+        if args.temperature == 1.0 and args.temperature_change == None:
             np.testing.assert_allclose(
                     prediction, scaled_prediction, atol=1e-5,
-                    err_msg='Prediction scaling at temperature=1.0 '
-                            'is not working as intended.')
+                    err_msg = 'Prediction scaling at temperature=1.0 is not working as intended.')
 
         sample = np.random.choice(
             np.arange(quantization_channels), p=scaled_prediction)
